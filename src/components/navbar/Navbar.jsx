3e7a7385ec3798a02d809fa2e2f7a2d673ff65e2
Anukriti <anukriti.student.che21@itbhu.ac.in>
2024-08-26 11:55:16 +0530

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     // logout function 
     const logout = () => {
         localStorage.clear('users');
-        navigate("/login")
+        navigate("/")
     }
 
     const cartItems = useSelector((state) => state.cart);
--- src/pages/registration/Signup.jsx
+++ src/pages/registration/Signup.jsx
@@ -4,8 +4,9 @@ import MyContext from '../../context/MyContext'
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth, fireDB } from '../../firebase/FirebaseConfig';
-import {addDoc, collection, Timestamp} from 'firebase/firestore';
+import {addDoc, collection, Timestamp, onSnapshot, query, where } from 'firebase/firestore';
 import Loader from '../../components/loader/Loader';
 
 function Signup() {
@@ -22,6 +23,12 @@ function Signup() {
         password: "",
         role: "user"
     })
+
+        //user login state
+        const [userLogin, setUserLogin] = useState({
+          email: "",
+          password: "",
+      })
 
     //users signup function
     const userSignupFunction = async () => {
@@ -72,6 +79,36 @@ function Signup() {
             setLoading(false);
         }
 
+    }
+        //guest account function
+      const handleGuestAccount = async() => {
+          const users = await signInWithEmailAndPassword(auth, "guestuser@gmail.com", "guest@123");
+          try {
+            const q = query(
+                collection(fireDB, "user"),
+                where('uid', '==', users?.user?.uid)
+            );
+            const data = onSnapshot(q, (QuerySnapshot) => {
+                let user;
+                QuerySnapshot.forEach((doc) => user = doc.data());
+                localStorage.setItem("users", JSON.stringify(user) )
+                setUserLogin({
+                    email: "",
+                    password: ""
+                })
+                toast.success("Login Successfully");
+                setLoading(false);
+                if(user.role === "user") {
+                    navigate('/');
+                }else{
+                    navigate('/admin-dashboard');
+                }
+            });
+            return () => data;
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
     }
 
   return (
@@ -160,7 +197,7 @@ function Signup() {
                   ></input>
                 </div>
               </div>
-              <div>
+              <div className='flex flex-col gap-5'>
                 <button
                   type="button"
                   onClick={userSignupFunction}
@@ -168,43 +205,16 @@ function Signup() {
                 >
                   Create Account <ArrowRight className="ml-2" size={16} />
                 </button>
+                <button
+                  type="button"
+                  onClick={handleGuestAccount}
+                  className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+                >
+                  Login with Guest User <ArrowRight className="ml-2" size={16} />
+                </button>
               </div>
             </div>
           </form>
-          {/* <div className="mt-3 space-y-3">
-            <button
-              type="button"
-              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-            >
-              <span className="mr-2 inline-block">
-                <svg
-                  className="h-6 w-6 text-rose-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                </svg>
-              </span>
-              Sign up with Google
-            </button>
-            <button
-              type="button"
-              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-            >
-              <span className="mr-2 inline-block">
-                <svg
-                  className="h-6 w-6 text-[#2563EB]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
-                </svg>
-              </span>
-              Sign up with Facebook
-            </button>
-          </div> */}
         </div>
       </div>
     </section>
