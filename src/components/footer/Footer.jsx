8863c14b5bd0956aff7c076baca885f971ea98ee
Anukriti <anukriti.student.che21@itbhu.ac.in>
2024-08-26 10:16:04 +0530

    update site name

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,14 +13,14 @@ const Footer = () => {
                     </a>
                     {/* para  */}
                     <p className="text-sm text-black sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-                        © 2024 kreteria —
+                        © 2024 kriteria —
                         <Link
                         to={'/'}
                             className="text-black ml-1"
                             rel="noopener noreferrer"
                             target="_blank"
                         >
-                            @kreteria
+                            @kriteria
                         </Link>
                     </p>
                     
